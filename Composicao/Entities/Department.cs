4b6217eceff91a363ce32de867123ac3cf0c9435
Felipe Alexandre <felipe.alexandre3388@gmail.com>
2023-06-09 18:34:49 -0300

    Exercício utilizando enumeracao e composição (primeira vez)

---
+++ Composicao/Entities/Department.cs
@@ -0,0 +1,17 @@
+﻿namespace Composicao.Entities
+{
+    public class Department
+    {
+        public string Name { get; set; }
+
+        public Department()
+        {
+        }
+
+        public Department(string name)
+        {
+            Name = name;
+        }
+        
+    }
+}
